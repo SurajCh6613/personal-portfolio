fc8ec868f07c740c6ccc2249404ad5e7dee952ac
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-04-14 16:54:34 +0530

    JS for professional skill circle

--- JS/script.js
+++ JS/script.js
@@ -39,3 +39,23 @@ changeText();
 setInterval(changeText,3000)
 
 // Changing text in home page JS end
+
+// Circle Skill JS Start
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 /dots;
+
+    for(let i =0; i< dots; i++){
+        points  += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i<percent; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+})
