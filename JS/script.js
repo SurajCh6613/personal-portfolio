0e5d358249530fa58fb4dbfa8d5da951f4ea8633
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-04-17 15:00:28 +0530

    JS for active Menu & Section

--- JS/script.js
+++ JS/script.js
@@ -62,3 +62,17 @@ circles.forEach(elem=>{
 
 // mix it up portfolio section js
 var mixer = mixitup('.portfolio-gallery');
+
+// Active menu JS
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 <section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
