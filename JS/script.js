9e51ba513ffb3c6cc647a6f14dd6b12a539969fb
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-04-17 17:45:38 +0530

    Making responsive and attractive

--- JS/script.js
+++ JS/script.js
@@ -82,3 +82,37 @@ const header = document.querySelector("header");
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY > 50)
 });
+
+// toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle("bx-x")
+    navlist.classList.toggle("open")
+}
+
+window.onscroll = () =>{
+    menuIcon.classList.remove("bx-x")
+    navlist.classList.remove("open")
+}
+
+// parallax
+const obeserver = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+        entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>obeserver.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>obeserver.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>obeserver.observe(el));
